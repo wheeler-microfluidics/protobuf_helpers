16f46e2a65f5f84d80ba6e95044bd4f5cf2b5517
Christian Fobel <christian@fobel.net>
2014-05-05 13:16:53 -0400

    Add underscore to camel-case conversion function

--- protobuf_helpers/__init__.py
+++ protobuf_helpers/__init__.py
@@ -6,13 +6,20 @@ from clang.cindex import TypeKind
 CLANG_TYPE_KIND_TO_PROTOBUF_TYPE = OrderedDict([
     (TypeKind.BOOL, 'bool'),
     (TypeKind.CHAR_S, 'sint32'),
+    (TypeKind.SCHAR, 'sint32'),
     (TypeKind.CHAR_U, 'uint32'),
     (TypeKind.FLOAT, 'float'),
     (TypeKind.INT, 'sint32'),
+    (TypeKind.LONG, 'sint32'),
     (TypeKind.LONGLONG, 'sint64'),
     (TypeKind.SHORT, 'sint32'),
     (TypeKind.UCHAR, 'uint32'),
     (TypeKind.UINT, 'uint32'),
     (TypeKind.ULONGLONG, 'uint64'),
     (TypeKind.USHORT, 'uint32'),
-    (TypeKind.VOID, None)])
+    (TypeKind.VOID, None),
+    ((TypeKind.POINTER, TypeKind.UCHAR), 'bytes')])
+
+
+def underscore_to_camelcase(value):
+    return ''.join(x.capitalize() if x else '_' for x in value.split('_'))
